290301c451cfe19983583e6ccf3c933c880d2694
Jorge Callisaya <sanchezenrique580@gmail.com>
2021-06-23 19:28:05 -0400

    Creacion de enpoints para listar tareas y obtener una tarea por ID

--- backend/src/controllers/tasks.js
+++ backend/src/controllers/tasks.js
@@ -1,8 +1,36 @@
-export const getTasks=(req,res)=>{
-    res.send('Hello World!!!')
+import {connect} from '../database'
+export const getTasks=async (req,res)=>{
+   try {
+        const db = await await connect();
+        const [rows]= await db.query('SELECT * FROM tasks')
+        res.json({
+            ok:true,
+            taks:rows
+        });
+   } catch (error) {
+       console.log(error);
+       res.json({
+        ok:false,
+        msg:'Algo salio mal!'
+    });
+   }
 }
-export const getTask=(req,res)=>{
-    res.send('Hello World!!!')
+export const getTask=async (req,res)=>{
+    const id = req.params['id'];
+   try {
+        const db = await await connect();
+        const [rows]= await db.query('SELECT * FROM tasks WHERE tasks.id = ?',[id])
+        res.json({
+            ok:true,
+            task:rows[0]
+        })
+   } catch (error) {
+       console.log(error);
+        res.json({
+            ok:false,
+            msg:'Algo salio mal!'
+        });
+   }
 }
 export const getTaskCount=(req,res)=>{
     res.send('Hello World!!!')
